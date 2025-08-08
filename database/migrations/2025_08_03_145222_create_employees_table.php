df31c64f80de4fc9b0292bc0846a78e3e6ae11d5
bidhannath2001 <bidhannath2001@gmail.com>
2025-08-09 02:15:53 +0600

    employee profile  management feature is developed

--- database/migrations/2025_08_03_145222_create_employees_table.php
+++ database/migrations/2025_08_03_145222_create_employees_table.php
@@ -13,6 +13,25 @@ return new class extends Migration
     {
         Schema::create('employees', function (Blueprint $table) {
             $table->id();
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->date('dob');
+            $table->string('gender');
+            $table->string('designation');
+            $table->string('department');
+            $table->date('joining_date');
+            $table->string('employment_type');
+            $table->string('salary_range');
+            $table->string('bonus_eligibility')->nullable();
+            $table->text('benefits')->nullable();
+            $table->string('phone');
+            $table->string('email');
+            $table->string('password');
+            $table->text('address');
+            $table->string('emergency_contact_name');
+            $table->string('emergency_contact_phone');
+            $table->string('resume')->nullable();
+            $table->string('id_proof')->nullable();
             $table->timestamps();
         });
     }
